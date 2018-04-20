0117e6c45ce3b4c2fcce7f13bc472dda8742dd82
Stepan Tytseiko <stepantytseyko@gmail.com>
2018-04-20 05:15:46 +0300

    Project Just to refresh java basic knovladges in my memory)

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by home on 20.04.2018.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
---
+++ src/Tutorials/IO/ByteArray/Test1.java
@@ -0,0 +1,54 @@
+package Tutorials.IO.ByteArray;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Test1 {
+    public static void main(String[] args) {
+        FileInputStream fis = null;
+        FileOutputStream fos = null;
+        try{
+            File file = new File("res\\IO\\input.txt");
+            if(file.exists()){
+                fis = new FileInputStream("res\\IO\\input.txt");
+                fos = new FileOutputStream("res\\IO\\output.txt",true);
+                int c;
+                byte[] b = new byte[2048];
+                int i = 0;
+
+                while((c=fis.read())!=-1){
+                    b[i]=(byte)c;
+                    i++;
+                    fos.write(c);
+                }
+                fos.write("\nThis is all\n".getBytes());
+                System.out.println(new String(b));
+//                System.out.println(Arrays.toString(b));
+            }else {
+                System.out.println("File \"res/IO/input.txt\" doesnt exist");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if(fis!=null){
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(fos!=null){
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            File outFile = new File("res\\IO\\output.txt");
+            if(outFile.exists())outFile.delete();
+        }
+    }
+}
